b98104e5757f82db832b5408b537b0cd87d0d198
Artem <artem@yugaev.ru>
2025-12-10 03:07:40 +0300

    pages added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,4 +13,11 @@
   padding: 0;
   box-sizing: border-box;
 }
+:root{
+  --color-main: #71557B;
+  --color-black: #0B0909;
+  --color-white: #F4F4F4;
+  --color-red: #AD0303;
+  --color-light-gray: #E2E2E2;
+}
 </style>
