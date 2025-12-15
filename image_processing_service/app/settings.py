fd54bbc3b6f78e995722040802936956665331c8
Artem <artem@yugaev.ru>
2025-12-16 00:52:12 +0300

    bugs fixed

--- image_processing_service/app/settings.py
+++ image_processing_service/app/settings.py
@@ -60,15 +60,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        "default": {
+    "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": "postgres",
         "PORT": 5432,
-    }
     }
 }
 
