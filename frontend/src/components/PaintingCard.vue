6a194203db9643e5218e21af4af02124ab650798
Artem <artem@yugaev.ru>
2025-12-18 02:07:48 +0300

    Update PaintingCard.vue

--- frontend/src/components/PaintingCard.vue
+++ frontend/src/components/PaintingCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="paintingCardWrapper">
     {{ painting }}
-    <img :src="'cipherbrush.ru' + painting.painting_colored" alt="painting" @error="handleImageError">
+    <img :src="getImageUrl()" alt="painting" @error="handleImageError">
     <div class="paintingCard-bottom">
         <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M1.73389 8.36012L6.86798 15.2602C8.34549 17.246 11.1545 17.246 12.632 15.2602L17.7661 8.36012C19.078 6.597 19.078 4.09172 17.7661 2.3286C15.8457 -0.252356 11.4071 0.514189 9.75 3.2981C8.09289 0.514189 3.65428 -0.252356 1.7339 2.3286C0.422036 4.09172 0.422033 6.597 1.73389 8.36012Z" stroke="#F4F4F4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -20,18 +20,8 @@ export default {
         }
     },
     computed: {
-        imageUrl() {
-            // Используем colored изображение или numbered, или оригинальное фото
-            if (this.painting.painting_colored) {
-                return this.painting.painting_colored;
-            }
-            if (this.painting.painting_numbered) {
-                return this.painting.painting_numbered;
-            }
-            if (this.painting.photo) {
-                return this.painting.photo;
-            }
-            return 'https://via.placeholder.com/300x300?text=No+Image';
+        getImageUrl() {
+            return 'https://cipherbrush.ru' + this.painting.painting_colored;
         },
     },
     methods: {
