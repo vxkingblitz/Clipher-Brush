529c73fed52ef070eaf73d187987e8694004de0d
Artem <artem@yugaev.ru>
2025-12-18 01:51:54 +0300

    Update PaintingCard.vue

--- frontend/src/components/PaintingCard.vue
+++ frontend/src/components/PaintingCard.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="paintingCardWrapper">
+    {{ painting }}
     <img :src="painting.painting_colored" alt="painting" @error="handleImageError">
     <div class="paintingCard-bottom">
         <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
