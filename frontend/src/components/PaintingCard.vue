16028044766603c56d0f398bcaf776696f6172b5
Artem <artem@yugaev.ru>
2025-12-18 02:24:56 +0300

    postgre port updated

--- frontend/src/components/PaintingCard.vue
+++ frontend/src/components/PaintingCard.vue
@@ -1,54 +1,53 @@
 <template>
-  <div class="paintingCardWrapper">
-    {{ painting }}
-    <img :src="imageUrl" alt="painting" @error="handleImageError">
-    <div class="paintingCard-bottom">
-        <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.73389 8.36012L6.86798 15.2602C8.34549 17.246 11.1545 17.246 12.632 15.2602L17.7661 8.36012C19.078 6.597 19.078 4.09172 17.7661 2.3286C15.8457 -0.252356 11.4071 0.514189 9.75 3.2981C8.09289 0.514189 3.65428 -0.252356 1.7339 2.3286C0.422036 4.09172 0.422033 6.597 1.73389 8.36012Z" stroke="#F4F4F4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-        <p class="nickname">{{ painting.username }}</p>
+    <div class="paintingCardWrapper">
+      <img :src="imageUrl" alt="painting" @error="handleImageError">
+      <div class="paintingCard-bottom">
+          <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1.73389 8.36012L6.86798 15.2602C8.34549 17.246 11.1545 17.246 12.632 15.2602L17.7661 8.36012C19.078 6.597 19.078 4.09172 17.7661 2.3286C15.8457 -0.252356 11.4071 0.514189 9.75 3.2981C8.09289 0.514189 3.65428 -0.252356 1.7339 2.3286C0.422036 4.09172 0.422033 6.597 1.73389 8.36012Z" stroke="#F4F4F4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          <p class="nickname">{{ painting.username }}</p>
+      </div>
     </div>
-  </div>
-</template>
-
-<script>
-export default {
-    props: {
-        painting: {
-            type: Object,
-            required: true
-        }
-    },
-    data() {
-        return {
-            imageUrl: null,
-        };
-    },
-    watch: {
-        painting: {
-            immediate: true,
-            handler(newVal) {
-                this.imageUrl = this.resolveImageUrl(newVal);
-            },
-        },
-    },
-    methods: {
-        resolveImageUrl(painting) {
-            if (!painting) return '';
-            if (painting.painting_colored && typeof painting.painting_colored === 'string') {
-                if (painting.painting_colored.startsWith('http')) {
-                    return painting.painting_colored;
-                }
-                return 'https://cipherbrush.ru' + painting.painting_colored;
-            }
-            return '';
-        },
-        handleImageError(e) {
-            e.target.src = 'https://via.placeholder.com/300x300?text=Error';
-        }
-    }
-}
-</script>
+  </template>
+  
+  <script>
+  export default {
+      props: {
+          painting: {
+              type: Object,
+              required: true
+          }
+      },
+      data() {
+          return {
+              imageUrl: null,
+          };
+      },
+      watch: {
+          painting: {
+              immediate: true,
+              handler(newVal) {
+                  this.imageUrl = this.resolveImageUrl(newVal);
+              },
+          },
+      },
+      methods: {
+          resolveImageUrl(painting) {
+              if (!painting) return '';
+              if (painting.painting_colored && typeof painting.painting_colored === 'string') {
+                  if (painting.painting_colored.startsWith('http')) {
+                      return painting.painting_colored;
+                  }
+                  return 'https://cipherbrush.ru' + painting.painting_colored;
+              }
+              return '';
+          },
+          handleImageError(e) {
+              e.target.src = 'https://via.placeholder.com/300x300?text=Error';
+          }
+      }
+  }
+  </script>
 
 <style scoped>
 .paintingCard-bottom{
